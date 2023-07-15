2aee7e98faeb867a931e1d2b7239a03470ee1d37
kssmp <115448106+kssmp@users.noreply.github.com>
2023-07-15 15:34:56 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -154,19 +154,6 @@ import {
         "Constructively oversaw a team of approximately 113 individuals therby curated and managed the institutes' annual cultural festival ABHIVYAKTI'23, which drew an impressive aggregate footfall of over 10,000 patrons",
       ],
     },
-    // {
-    //   title: "Full stack Developer",
-    //   company_name: "Meta",
-    //   icon: meta,
-    //   iconBg: "#E6DEDD",
-    //   date: "Jan 2023 - Present",
-    //   points: [
-    //     "Developing and maintaining web applications using React.js and other related technologies.",
-    //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-    //     "Implementing responsive design and ensuring cross-browser compatibility.",
-    //     "Participating in code reviews and providing constructive feedback to other developers.",
-    //   ],
-    // },
   ];
   
   const testimonials = [
